328213da4b55283ce6e8a3a19ce4e9929b0792f0
David Kuryakin <dkuryakin@gmail.com>
2022-04-23 03:57:11 +0300

    fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-go build -a -ldflags "-linkmode external -extldflags '-static' -s -w -X main.sha1ver=`git rev-parse HEAD` -o main main.go
+go build -a -ldflags "-linkmode external -extldflags '-static' -s -w -X main.sha1ver=`git rev-parse HEAD`" -o main main.go
